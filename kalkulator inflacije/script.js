679e7f53b156449c23da1dddcb0267f8e0c606fe
Jusuf Misic <jusuf.gv1@gmail.com>
2023-10-26 11:02:01 +0200

    update

--- kalkulator inflacije/script.js
+++ kalkulator inflacije/script.js
@@ -1,26 +1,23 @@
 function inflationCalculator() {
-    let inflationRate=document.querySelector("#inflationRate");
-    let money=document.querySelector("#money");
-    let years=document.querySelector('#years').value;
+    let inflationRate = document.querySelector("#inflationRate");
+    let money = document.querySelector("#money");
+    let years = document.querySelector("#years").value;
 
-    inflationRate=parseFloat(inflationRate.value);
-    money=parseFloat(money.value);
-    years=parseFloat(years);
+    inflationRate = parseFloat(inflationRate.value);
+    money = parseFloat(money.value);
+    years = parseFloat(years);
 
-    let worth=money+(money*(inflationRate/100));
+    let worth = money + money * (inflationRate / 100);
 
-    for(let i=1; i<years; i++){
-        worth+=worth*(inflationRate/100);
+    for (let i = 1; i < years; i++) {
+        worth += worth * (inflationRate / 100);
     }
 
     worth = worth.toFixed(2);
 
-    let newElement=document.createElement('div');
-    newElement.className='new-value';
-    newElement.innerText=`Danasnjih ${money}e vrijedi isto kao ${worth}e za ${years} godina`;
+    let newElement = document.createElement("div");
+    newElement.className = "new-value";
+    newElement.innerText = `Danasnjih ${money}e vrijedi isto kao ${worth}e za ${years} godina`;
 
-    document.querySelector('.container').appendChild(newElement);
-
- 
-
+    document.querySelector(".container").appendChild(newElement);
 }
